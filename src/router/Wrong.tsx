c409ce37c20d0a98b221dc4013b1a87fed67a588
Trung <nqt230103a@gmail.com>
2024-05-15 16:05:12 +0700

    create page-404

--- src/router/Wrong.tsx
+++ src/router/Wrong.tsx
@@ -1,10 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
+
 export default function Wrong() {
   return (
-    <div className="flex justify-center w-full h-[80vh]">
-      <img
-        src="https://cloud.z.com/vn/wp-content/uploads/2023/04/loi-404-not-found-la-gi-9-cach-khac-phuc-loi-404-not-5-800x450-1.jpg"
-        alt="wrong-page"
-      />
+    <div className="mx-[140px] site-body py-[250px] pl-[500px] bg-left bg-no-repeat" style={{
+      backgroundImage: "url('https://static-cms-giaoducthoidai.epicdn.me/v1/web/styles/img/bg-404.png')",
+      backgroundSize: '1022px'
+    }}>
+      <div className=" flex flex-col justify-center items-center text-center">
+        <div className="wrap-content">
+          <p className="title text-[26px] font-bold mb-4">Không tìm thấy đường dẫn này</p>
+          <p className="des font-bold text-gray-600 italic text-[14px] mb-7">
+            Bạn có thể truy cập vào <a href="/" className="home text-[#333333]">Trang chủ</a> hoặc sử dụng <br /> ô dưới đây để tìm kiếm
+          </p>
+        </div>
+        <div className="wrap-search my-4 w-[596px] max-w-[600px] relative">
+          <input
+            type="text"
+            className="bg-[#f8f9fa] txtsearch p-2 border border-gray-300 rounded-md w-full h-[64px] focus:outline-none"
+            placeholder="Tìm kiếm thông tin"
+          />
+          <div className="absolute right-1.5 top-1/2 transform -translate-y-1/2 px-4 py-3.5 bg-[#e21f26] rounded-[5px] hover:cursor-pointer">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="text-[24px] text-white" />
+          </div>
+        </div>
+        <a href="/" className="block w-[130px] h-[38px] bg-[#ce2e0e] rounded-[3px] font-medium text-[16px] leading-[19px] text-white px-[18px] py-[8px]">TRANG CHỦ</a>
+      </div>
     </div>
   );
 }
