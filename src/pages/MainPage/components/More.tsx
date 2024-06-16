bae91262dbcc117293a90fafd7d7982cb7424e43
Nguyễn Quốc Trung <118261990+nicha2301@users.noreply.github.com>
2024-06-16 23:15:31 +0700

    Update More.tsx

--- src/pages/MainPage/components/More.tsx
+++ src/pages/MainPage/components/More.tsx
@@ -4,13 +4,13 @@ export default function More() {
   return (
     <>
       <div className='flex items-center justify-center mt-[30px]'>
-        <button className='py-3 px-12 rounded-full border border-gray-500'>XEM THÊM</button>
+        <button className='text-gray-700 py-3 px-12 rounded-full border border-gray-500'>XEM THÊM</button>
       </div>
-      <div className='flex items-center md:flex-row flex-col bg-primaryColor rounded-t-2xl font-bold text-[16px] '>
-        <div className='flex-1 py-4 text-center md:border-r border-r-white'>GIÁO DỤC</div>
-        <div className='flex-1 py-4 text-center md:border-r border-r-white'>THỜI SỰ</div>
-        <div className='flex-1 py-4 text-center md:bborder-r border-r-white'>GIÁO DỤC PHÁP LUẬT</div>
-        <div className='flex-1 py-4 text-center md:bborder-r border-r-white'>KẾT NỐI</div>
+      <div className='flex items-center bg-primaryColor rounded-t-2xl text-white font-bold text-[16px] text-[#FFFFFF]'>
+        <div className='flex-1 py-4 text-center border-r border-r-white'>GIÁO DỤC</div>
+        <div className='flex-1 py-4 text-center border-r border-r-white'>THỜI SỰ</div>
+        <div className='flex-1 py-4 text-center border-r border-r-white'>GIÁO DỤC PHÁP LUẬT</div>
+        <div className='flex-1 py-4 text-center border-r border-r-white'>KẾT NỐI</div>
         <div className='flex-1 py-4 text-center'>MEDIA</div>
       </div>
     </>
