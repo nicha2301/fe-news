8f2282cda64c64326af57be3126a3108b3928652
Lê Quốc Trung <115451384+Danny2611@users.noreply.github.com>
2024-07-08 00:52:33 +0700

    delete historyReadArticle

--- src/pages/ReadingHistoryPage/ReadingHistoryPage.tsx
+++ src/pages/ReadingHistoryPage/ReadingHistoryPage.tsx
@@ -16,14 +16,18 @@ export const ReadingHistoryPage = () => {
       setReadArticles(JSON.parse(storedArticles));
     }
   }, []);
-
+  const removeArticle = (link: string) => {
+    const updatedArticles = readArticles.filter(article => article.link !== link);
+    setReadArticles(updatedArticles);
+    localStorage.setItem("readArticles", JSON.stringify(updatedArticles));
+  };
  
 
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-2xl font-bold mb-4">Lịch sử các bài báo đã đọc</h1>
       {readArticles.length > 0 ? (
-        <ListArticle data={readArticles} header={false} />
+        <ListArticle data={readArticles} header={false}  onRemoveArticle={removeArticle} />
       ) : (
         <p className="text-gray-500">Bạn chưa đọc bài báo nào.</p>
       )}
