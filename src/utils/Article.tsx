35f428015a7203d94512ade935f138bf34577262
namthuan2304 <thuandangnam@gmail.com>
2024-07-09 15:23:48 +0700

    back to top

--- src/utils/Article.tsx
+++ src/utils/Article.tsx
@@ -1044,9 +1044,6 @@ const cssContent = `
 export const Article: React.FC<{ url: string }> = (props) => {
   const [contents, setContents] = useState<{ selector: string, html: string }[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [isReading, setIsReading] = useState<boolean>(false)
-  const [voice, setVoice] = useState<SpeechSynthesisVoice | null>(null)
-  const [utterance, setUtterance] = useState<SpeechSynthesisUtterance | null>(null)
   const selectors = ['.box-social', '.article'];
 
   useEffect(() => {
@@ -1072,44 +1069,8 @@ export const Article: React.FC<{ url: string }> = (props) => {
     fetchData();
   }, [props.url]);
 
-  useEffect(() => {
-    const getVoices = () => {
-       const voices = window.speechSynthesis.getVoices()
-       const vietnameseVoice = voices.find((voice) => voice.lang === 'vi-VN')
-       setVoice(vietnameseVoice || null)
-    }
 
-    if (window.speechSynthesis.onvoiceschanged !== undefined) {
-       window.speechSynthesis.onvoiceschanged = getVoices
-    }
-    getVoices()
- }, [])
 
- const handleReadAloud = () => {
-    const speechSynthesis = window.speechSynthesis
-    if (isReading) {
-       speechSynthesis.pause()
-       setIsReading(false)
-    } else {
-       if (utterance) {
-          speechSynthesis.resume()
-       } else {
-          if (!voice) {
-             alert('Vietnamese voice is not available on this system.')
-             return
-          }
-          const newUtterance = new SpeechSynthesisUtterance(
-             DOMPurify.sanitize(contents[1].html, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] })
-          )
-          newUtterance.lang = 'vi-VN'
-          newUtterance.voice = voice
-          newUtterance.onend = () => setIsReading(false)
-          speechSynthesis.speak(newUtterance)
-          setUtterance(newUtterance)
-       }
-       setIsReading(true)
-    }
- }
 
 
   if (loading) {
@@ -1149,54 +1110,6 @@ export const Article: React.FC<{ url: string }> = (props) => {
   
 </button>
 
-
-      <Button onClick={handleReadAloud} className='flex items-center justify-center gap-x-1'>
-               {isReading ? (
-                  <>
-                     <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        strokeWidth={1.5}
-                        stroke='currentColor'
-                        className='size-6'
-                     >
-                        <path
-                           strokeLinecap='round'
-                           strokeLinejoin='round'
-                           d='M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z'
-                        />
-                     </svg>
-                     Tạm dừng
-                  </>
-               ) : (
-                  <>
-                     <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        strokeWidth={1.5}
-                        stroke='currentColor'
-                        className='size-6'
-                     >
-                        <path
-                           strokeLinecap='round'
-                           strokeLinejoin='round'
-                           d='M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z'
-                        />
-                     </svg>
-                     Đọc tự động
-                  </>
-               )}
-               <div id='clip'>
-                  <div id='leftTop' className='corner' />
-                  <div id='rightBottom' className='corner' />
-                  <div id='rightTop' className='corner' />
-                  <div id='leftBottom' className='corner' />
-               </div>
-               <span id='rightArrow' className='arrow' />
-               <span id='leftArrow' className='arrow' />
-            </Button>
       {contents[1].html ?
         contents.map(({ selector, html }) => (
           <div key={selector} className={selector.replace('.', '') + ' sticky-top'} dangerouslySetInnerHTML={{ __html: html }} />
